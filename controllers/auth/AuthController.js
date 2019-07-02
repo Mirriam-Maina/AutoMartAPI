0f9986f52a6f06dd9953f844597c805778d3d4f4
Mirriam Maina <mirriam@Mirriams-MacBook-Pro.local>
2019-07-02 16:42:34 +0200

    feat(user-signin): a user should be able to log in to the application
    - a user should use email and password to log in
    - validation for email and password
    - check  if user exists
    - error if user does not exist or incorrect email and password
    
    [Finishes #167041520]

--- controllers/auth/AuthController.js
+++ controllers/auth/AuthController.js
@@ -9,19 +9,34 @@ export default class AuthController {
         const { email, firstName, lastName, password, address } = req.body;
         let jwtToken = signToken({email, firstName, lastName});
         let newUser = new User(firstName, lastName, email, password, address);
-        let createdUser = await newUser.addUser()
-        if (createdUser === 'User exists'){
-            return res.status(409).json({
-            status: 409,
-            message: "This user already exists. Log in instead",
-        })
-        }
+        let createdUser = await newUser.addUser();
         return res.status(201).json({
             status: 201,
             message: "User successfully created",
             data: omit(createdUser, 'password'),
             token: jwtToken
         })
+    }
+
+    static async signin(req, res){
+        const { email, password } = req.body;
+        let signInUser = await User.signInUser(email, password)
+        let jwtToken = signToken({email});
+        console.log(signInUser)
+        if (signInUser){
+            return res.status(200).json({
+                status: 200,
+                message: "User successfully logged in",
+                data: omit(signInUser, 'password'),
+                token: jwtToken
+            })
+        }
+        else if(signInUser === false){
+            return res.status(400).json({
+                status: 400,
+                error: "Incorrect username or password",
+            })
+        }
     }
 }
 
